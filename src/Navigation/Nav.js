d58bb44b950f25a8abb42710ae86f0c7df34a9c1
Prince Sharma <ps789650@gmail.com>
2024-01-12 22:08:07 +0530

    made basic filtering

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,22 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import "./Nav.css";
+
+const Nav = ({ handleInputChange, query }) => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Enter your search shoes."
+        />
+      </div>
+      
+    </nav>
+  );
+};
+
+export default Nav;
